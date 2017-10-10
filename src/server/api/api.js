02c75bab3156acd30be6f2eee1ba168065c30158
Sławomir Jopek <pwnsoon@gmail.com>
2017-10-10 18:27:08 +0200

    api update

--- src/server/api/api.js
+++ src/server/api/api.js
@@ -13,31 +13,40 @@ app.use(bodyParser.json());
 mongoose.connect(`mongodb://localhost:27017/guestbook`);
 
 app.get("/guestbook", (req, res) => {
-    const post = req.body;
-    posts.find(post, (err, data) => {
+    posts.find((err, data) => {
         if (err) throw err;
         res.json(data);
     });
 });
 
 app.get("/guestbook/:id", (req, res) => {
-    // get specific entry
+    const query = {_id: req.params.id};
+
+    posts.find(query, (err, data) => {
+        if (err) throw err;
+        res.json(data);
+    });
 });
 
-app.get("/guestbook/category/", (req, res) => {
-    // get all entries from category
+app.get("/guestbook/category/:category", (req, res) => {
+    // @TODO get all entries from category
 });
 
 app.get("/guestbook/archive/:month", (req, res) => {
-    // get all entries from selected month
+    // @TODO get all entries from selected month
 });
 
 app.delete("/guestbook/:id", (req, res) => {
-    // delete specific entry
+    const query = {_id: req.params.id};
+
+    posts.remove(query, (err, data) => {
+        if (err) throw err;
+        res.json(data);
+    });
 });
 
 app.put("/guestbook/:id", (req, res) => {
-    // add new entry
+    // @TODO add new entry
 });
 
 const port = process.env.PORT || 5000;
