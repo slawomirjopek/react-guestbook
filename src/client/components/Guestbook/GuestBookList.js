685bb66836cf1d79cdb480ef253ef8370fa38b99
Sławomir Jopek <pwnsoon@gmail.com>
2017-10-20 02:16:35 +0200

    styling... more

--- src/client/components/Guestbook/GuestBookList.js
+++ src/client/components/Guestbook/GuestBookList.js
@@ -1,29 +1,32 @@
 import React, {Component} from "react";
+import { Jumbotron } from "reactstrap";
 
 const GuestBookList = (props) => {
     const entries = props.entries.map((entry, key) => (
-        <li key={key}>
+        <Jumbotron key={key} className="px-4 py-4">
             <header>
-                <h3>{entry.title}</h3>
+                <h3 className="display-4">{entry.title}</h3>
                 <span className="author">{entry.author}</span>
                 <span className="date">{entry.date}</span>
             </header>
 
+            <hr className="my-2" />
+
             <div className="content">
-                {entry.content}
+                <p className="lead">{entry.content}</p>
             </div>
+
+            <hr className="my-2" />
 
             <div className="tags">
                 {entry.tags}
             </div>
-        </li>
+        </Jumbotron>
     ));
 
     return (
         <div>
-            <ul>
-                {entries}
-            </ul>
+            {entries}
         </div>
     )
 };
