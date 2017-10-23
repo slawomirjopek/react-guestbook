da21fc7796bc927fa4f52fc5595c8b4071d95984
Sławomir Jopek <pwnsoon@gmail.com>
2017-10-23 14:40:35 +0200

    Login form

--- src/client/components/Login/LoginForm.js
+++ src/client/components/Login/LoginForm.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 import { Form, FormGroup, Input, Button } from "reactstrap";
+const _ = require("lodash");
 
 class LoginForm extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            fields: {
+                login: {
+                    value: "",
+                    valid: false,
+                    errors: []
+                },
+                password: {
+                    value: "",
+                    valid: false,
+                    errors: []
+                }
+            },
+            formValid: false
+        }
+    }
+
     render() {
         return (
-            <Form>
+            <Form
+                onSubmit={this.submitHandler.bind(this)}
+                onChange={this.changeHandler.bind(this)}
+            >
                 <FormGroup>
                     <Input
                         name="login"
@@ -19,9 +43,72 @@ class LoginForm extends Component {
                         placeholder="password"
                     />
                 </FormGroup>
-                <Button type="submit">Log in</Button>
+                <Button type="submit" disabled={!this.state.formValid}>Log in</Button>
             </Form>
         )
+    }
+
+    submitHandler(e) {
+        e.preventDefault();
+        const data = this.getFormData(e.target);
+
+        if (this.state.formValid) {
+            // login
+        }
+    }
+
+    updateField(fieldName, data, callback) {
+        const currentField = { ...this.state.fields[fieldName], ...data };
+        const updatedFields = { ...this.state.fields, [fieldName]: currentField };
+
+        this.setState({ fields: updatedFields }, callback);
+    }
+
+    getFormData(form) {
+        const fromData = new FormData(form);
+        return {
+            login: fromData.get("login"),
+            password: fromData.get("password")
+        }
+    }
+
+    changeHandler(e) {
+        const field = e.target;
+        this.updateField(field.name, { value: field.value }, () => {
+            this.validateField(field.name)
+        });
+    }
+
+    validateField(fieldName) {
+        let valid = false;
+        let errors = [];
+        let value = this.state.fields[fieldName].value;
+
+        switch (fieldName) {
+            case "login":
+                valid = new RegExp(/[0-9a-zA-Z]{6,}$/i).test(value);
+                if (!valid) errors.push("Name to short!");
+                break;
+            case "password":
+                valid = new RegExp(/[0-9a-zA-Z]{6,}$/i).test(value);
+                if (!valid) errors.push("Title to short!");
+                break;
+            default:
+                break;
+        }
+
+        this.updateField(fieldName, { value, valid, errors }, () => {
+            this.validateForm()
+        })
+    }
+
+    validateForm() {
+        const valid = _.filter(this.state.fields, { valid: false });
+        this.setState({
+            formValid: valid.length ? false : true
+        }, () => {
+            console.log(this.state)
+        });
     }
 }
 
--- src/client/components/Pages/LoginPage.js
+++ src/client/components/Pages/LoginPage.js
@@ -5,6 +5,7 @@ import LoginForm from "../../components/Login/LoginForm";
 const LoginPage = () => (
     <Row>
         <Col xs="12" sm="10" md="8" lg="6" >
+            <h2>Admin login</h2>
             <LoginForm/>
         </Col>
     </Row>
