705e392b6eb4e16f291be7079a495aeba659eb35
Sławomir Jopek <pwnsoon@gmail.com>
2017-10-27 15:49:56 +0200

    storage service finished

--- src/client/services/storage.js
+++ src/client/services/storage.js
@@ -1,6 +1,5 @@
-import { store } from "../store/store";
-import loginActions from "../actions/login";
 import TYPES from "../types/storage";
+const _ = require("lodash");
 
 class Storage {
     constructor() {
@@ -22,23 +21,46 @@ class Storage {
     }
 
     set(data) {
-        // object { key: value }
-        // array of objects { key: value }
+        if (!_.isPlainObject(data)) return;
 
-        sessionStorage.setItem("test", data);
+        if (Object.keys(data).length) {
+            _.forEach(data, (value, key) => this.storage.setItem(key, value));
+        }
     }
 
     get(key) {
         if (!key || typeof key !== "string") {
-            throw new TypeError(`Storage get should be string`);
+            throw new TypeError(`Storage get param should be non-empty string`);
         }
 
         return this.storage.getItem(key);
     }
 
-    check(data) {
-        // object { key: value }
-        // return boolean
+    // is exists key:value in storage
+    isExists(data) {
+        if (!_.isPlainObject(data)) return false;
+
+        let exists = false;
+
+        if (Object.keys(data).length && Object.keys(data).length < 2) {
+            _.forEach(data, (value, key) => {
+                if (this.get(key) === value) exists = true;
+            });
+        }
+
+        return exists;
+    }
+
+    remove(key) {
+        if (!key || typeof key !== "string") {
+            throw new TypeError(`Storage remove param should be non-empty string`);
+        }
+
+        this.storage.removeItem(key);
+    }
+
+    clear() {
+        this.storage.clear();
     }
 }
 
