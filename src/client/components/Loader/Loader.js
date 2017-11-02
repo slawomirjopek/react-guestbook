1eba656e720c33d06c971e2dfb8d524cc37c2b2a
Sławomir Jopek <pwnsoon@gmail.com>
2017-11-02 13:52:16 +0100

    spinner body update

--- src/client/components/Loader/Loader.js
+++ src/client/components/Loader/Loader.js
@@ -2,21 +2,16 @@ import React, { Component } from "react";
 import "./loader.scss";
 
 const Loader = () => {
+    const spinnerBody = [];
+
+    while (spinnerBody.length < 12) {
+        spinnerBody.push(<div></div>)
+    }
+
     return (
         <div className="lds-css">
             <div className="lds-spinner">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                {spinnerBody}
             </div>
         </div>
     )
